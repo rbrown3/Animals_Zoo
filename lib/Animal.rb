a8915dffdb7695b5be4d427821c58f619957504b
rbrown3 <rosannabrown@outlook.com>
2018-12-13 14:47:00 -0500

    final commit

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Animal
-
+  @@all =[]
+  attr_accessor :weight
+  attr_reader :species, :nickname, :zoo
+  def initialize(species,nickname,weight, zoo)
+    @species=species
+    @nickname=nickname
+    @weight=weight
+    @@all << self
+    @zoo = zoo
+  end
+  def self.all
+    @@all
+  end
+  def self.find_by_species(wanted_species)
+    @@all.select do |animal|
+     animal.species == wanted_species
+   end
+ end
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,44 @@
-class Zoo
+require 'pry'
 
+class Zoo
+  @@all = []
+  attr_accessor :location, :name
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+#class method. Returns an array of all zoos
+  def self.find_by_location(wanted_location)
+    @@all.select do |zoo|
+      zoo.location == wanted_location
+    end
+  end
+
+  def animals
+    Animal.all.select do |animal|
+      self == animal.zoo
+    end
+  end
+
+  def animal_species
+    array = []
+    self.animals.each do |animal|
+      array << animal.species
+    end
+    array.uniq!
+  end
+
+  def animal_nicknames
+    array = []
+    self.animals.each do |animal|
+      array << animal.nickname
+    end
+    array
+  end
 end
